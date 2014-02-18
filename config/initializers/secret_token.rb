81cba975fee16bc74d122efadec05544b67391d6
Andrew Kimn <akimn89@gmail.com>
2014-02-18 05:37:22 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Layprophet::Application.config.secret_key_base = '80aaf046e6d5aa666ee2b62979c9a332c7fed62d88045fa9f260ceded70e92e7f85cc639975ffa491abb79ab12a42258386d38c1028c4fb715143301686aa07b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Layprophet::Application.config.session_store :cookie_store, key: '_layprophet_session'
