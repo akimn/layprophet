e8c47373919a61eff52247855ac62f0d871b7e63
Andrew Kimn <akimn89@gmail.com>
2014-02-22 00:00:36 -0800

    stuff

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -21,28 +21,39 @@ class PinsController < ApplicationController
 
   def create
     @pin = current_user.pins.build(pin_params)
-    if @pin.save
-      redirect_to @pin, notice: 'Pin was successfully created.'
-    else
-      render action: 'new'
+
+    respond_to do |format|
+      if @pin.save
+        format.html { redirect_to @pin }
+        format.json { render action: 'show', status: :created, location: @pin }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @pin.errors, status: :unprocessable_entity }
+      end
     end
   end
 
   def update
-    if @pin.update(pin_params)
-      redirect_to @pin, notice: 'Pin was successfully updated.'
-    else
-      render action: 'edit'
+    respond_to do |format|
+      if @pin.update(pin_params)
+        format.html { redirect_to @pin }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @pin.errors, status: :unprocessable_entity }
+      end
     end
   end
 
   def destroy
     @pin.destroy
-    redirect_to pins_url
+    respond_to do |format|
+      format.html { redirect_to pins_url }
+      format.json { head :no_content }
+    end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_pin
       @pin = Pin.find(params[:id])
     end
