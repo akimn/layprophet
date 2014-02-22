4f9fde78b4efec624158c9a1b6f321ce98c7c231
Andrew Kimn <akimn89@gmail.com>
2014-02-22 00:03:23 -0800

    stuff

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -10,20 +10,19 @@ class Pin < ActiveRecord::Base
 	
 	validates :title_of_piece, length: { minimum: 1 }
  	validates :title_of_piece, length: { maximum: 255 }
-	validates :brand length: { minimum: 1 }
-  	validates :brand length: { maximum: 255 }
+	validates :brand, length: { minimum: 1 }
+  	validates :brand, length: { maximum: 255 }
   	validates :comment, length: { minimum: 1 }
   	validates :comment, length: { maximum: 255 }
 
   	validates :password, length: { in: 6..40 }
-end
 
 
 
 	def self.search(search)
 		
 	  if search	
-      	where('comment ILIKE ? OR "church_name" ILIKE ? OR "pastor_or_worship_leader_name" ILIKE ?', "%#{search}%", "%#{search}%", "%#{search}%")
+      	where('title_Of_Piece ILIKE ? OR "brand" ILIKE ? OR "comment" ILIKE ?', "%#{search}%", "%#{search}%", "%#{search}%")
 	  else
 	    scoped
 	  end
