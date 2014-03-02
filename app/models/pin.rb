38a2dfc85adf056abe9df1a5f8404c92f294adeb
Andrew Kimn <akimn89@gmail.com>
2014-03-01 16:59:43 -0800

    stuff

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -21,7 +21,7 @@ class Pin < ActiveRecord::Base
 	def self.search(search)
 		
 	  if search	
-      	where('comment ILIKE ? OR brand ILIKE ? OR title_of_piece ILIKE ?', "%#{search}%", "%#{search}%", "%#{search}%")
+      	where('comment ILIKE ? OR brand ILIKE ? OR "title_of_piece" ILIKE ?', "%#{search}%", "%#{search}%", "%#{search}%")
 	  else
 	    scoped
 	  end
