064785aa861591a804cf0948d24f7df99ae9d6a2
Andrew Kimn <akimn89@gmail.com>
2014-03-01 16:55:45 -0800

    stuff

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140223233444) do
+ActiveRecord::Schema.define(version: 20140222020131) do
 
   create_table "pins", force: true do |t|
     t.string   "title_Of_Piece"
