440936d406ffab05700622ddc63993eede473fe5
Andrew Kimn <akimn89@gmail.com>
2014-03-20 19:16:53 -0700

    stuff

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -21,41 +21,45 @@ class PinsController < ApplicationController
   end
 
   def create
-    @pin = current_user.pins.build(pin_params)
-    if @pin.save
-      redirect_to @pin, notice: 'Pin was successfully created.'
-    else
-      render action: 'new'
-    end
     #@pin = current_user.pins.build(pin_params)
-
-    #respond_to do |format|
-     # if @pin.save
-      #  format.html { redirect_to @pin }
-        ##format.html { redirect_to @pin, notice: 'Post was successfully created.' }
-       # format.json { render action: 'show', status: :created, location: @pin }
-      #else
-       # format.html { render action: 'new' }
-        #format.json { render json: @pin.errors, status: :unprocessable_entity }
-      #end
+    #if @pin.save
+     # redirect_to @pin, notice: 'Pin was successfully created.'
+    #else
+     # render action: 'new'
     #end
+
+
+    @pin = current_user.pins.build(pin_params)
+
+    respond_to do |format|
+      if @pin.save
+        format.html { redirect_to @pin }
+        #format.html { redirect_to @pin, notice: 'Post was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @pin }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @pin.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def update
-    if @pin.update(pin_params)
-      redirect_to @pin, notice: 'Pin was successfully updated.'
-    else
-      render action: 'edit'
-    end
-    #respond_to do |format|
-     # if @pin.update(pin_params)
-      #  format.html { redirect_to @pin }
-       # format.json { head :no_content }
-      #else
-       # format.html { render action: 'edit' }
-       # format.json { render json: @pin.errors, status: :unprocessable_entity }
-      #end
+    #if @pin.update(pin_params)
+     # redirect_to @pin, notice: 'Pin was successfully updated.'
+    #else
+     # render action: 'edit'
     #end
+
+
+    respond_to do |format|
+      if @pin.update(pin_params)
+        format.html { redirect_to @pin }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @pin.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
