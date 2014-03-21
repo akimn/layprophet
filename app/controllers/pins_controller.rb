f17103a9068bbdd32511919c244a0b95c9523843
Andrew Kimn <akimn89@gmail.com>
2014-03-20 19:12:35 -0700

    stuff

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -22,29 +22,40 @@ class PinsController < ApplicationController
 
   def create
     @pin = current_user.pins.build(pin_params)
-
-    respond_to do |format|
-      if @pin.save
-        format.html { redirect_to @pin }
-        #format.html { redirect_to @pin, notice: 'Post was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @pin }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @pin.errors, status: :unprocessable_entity }
-      end
+    if @pin.save
+      redirect_to @pin, notice: 'Pin was successfully created.'
+    else
+      render action: 'new'
     end
+    #@pin = current_user.pins.build(pin_params)
+
+    #respond_to do |format|
+     # if @pin.save
+      #  format.html { redirect_to @pin }
+        ##format.html { redirect_to @pin, notice: 'Post was successfully created.' }
+       # format.json { render action: 'show', status: :created, location: @pin }
+      #else
+       # format.html { render action: 'new' }
+        #format.json { render json: @pin.errors, status: :unprocessable_entity }
+      #end
+    #end
   end
 
   def update
-    respond_to do |format|
-      if @pin.update(pin_params)
-        format.html { redirect_to @pin }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @pin.errors, status: :unprocessable_entity }
-      end
+    if @pin.update(pin_params)
+      redirect_to @pin, notice: 'Pin was successfully updated.'
+    else
+      render action: 'edit'
     end
+    #respond_to do |format|
+     # if @pin.update(pin_params)
+      #  format.html { redirect_to @pin }
+       # format.json { head :no_content }
+      #else
+       # format.html { render action: 'edit' }
+       # format.json { render json: @pin.errors, status: :unprocessable_entity }
+      #end
+    #end
   end
 
   def destroy
@@ -61,8 +72,8 @@ class PinsController < ApplicationController
     end
 
     def correct_user
-      @pin = current_user.pin.find_by(id: params[:id])
-      redirect_to pins_path, notice: "Not authorized to edit this post" if @pin.user.nil?
+      @pin = current_user.pins.find_by(id: params[:id])
+      redirect_to pins_path, notice: "Not authorized to edit this post" if @pin.nil?
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
