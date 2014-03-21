3ecad85446367b04cf64c89311bc41ceb904288d
Andrew Kimn <akimn89@gmail.com>
2014-03-20 18:38:06 -0700

    stuff

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -62,7 +62,7 @@ class PinsController < ApplicationController
 
     def correct_user
       @pin = current_user.pin.find_by(id: params[:id])
-      redirect_to pins_path, notice: "Not authorized to edit this post" unless @pin.user.nil?
+      redirect_to pins_path, notice: "Not authorized to edit this post" if @pin.user.nil?
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
